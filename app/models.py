68e6d53835389240c0a350027ecc185c4d1ef0b0
mmasser95 <mmasser95@gmail.com>
2019-09-11 20:35:23 +0200

    Formateo y creación de la tabla relacion

--- app/models.py
+++ app/models.py
@@ -3,63 +3,73 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    admin=db.Column(db.Boolean)
     password_hash = db.Column(db.String(128))
-    typ=db.Column(db.String(10), default="user")
-    grupo=db.Column(db.ForeignKey('grupo.id'))
+    typ = db.Column(db.String(10), default="user")
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+
     def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name!= 'password_hash'}
-       
-    def set_pass(self,p):
-        self.password_hash=generate_password_hash(p)
-    
-    def check_pass(self,p):
+        return {
+            c.name: getattr(self, c.name)
+            for c in self.__table__.columns if c.name != 'password_hash'
+        }
+
+    def set_pass(self, p):
+        self.password_hash = generate_password_hash(p)
+
+    def check_pass(self, p):
         return check_password_hash(self.password_hash, p)
 
 
-class Grupo(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    nombre=db.Column(db.String(64), index=True, unique=True)
+class UserPermsRecurso(db.Model):
+    userId = db.Column(db.ForeignKey('user.id'), primary_key=True)
+    permsId = db.Column(db.ForeignKey('perms.id'), nullable=False)
+    recursoId = db.Column(db.ForeignKey('recurso.id'), primary_key=True)
 
-    
-class Perms(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    grupo=db.Column(db.Integer, db.ForeignKey('grupo.id'))
-    recursoId=db.Column(db.Integer, db.ForeignKey('recurso.id'))
-    view=db.Column(db.Boolean)
-    opn=db.Column(db.Boolean)
-    admn=db.Column(db.Boolean)
-    
     def __repr__(self):
-        return f'<Perms {self.userId} --> {self.recursoId}>'
-    
+        return f'<User  {self.userId} --> Perms {self.permsId} --> Recurso {self.recursoId}>'
+
     def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Perms(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    view = db.Column(db.Boolean, nullable=False)
+    opn = db.Column(db.Boolean, nullable=False)
+    admn = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f'<Perms {self.userId}>'
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class Recurso(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    nombre=db.Column(db.String(64), nullable=False)
-    userId=db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
-    permsId=db.Column(db.Integer, db.ForeignKey('perms.id'),nullable=False)
-    tipo=db.Column(db.String(10), nullable=False)
-    pin=db.Column(db.String(10),nullable=False)
-    tActivo=db.Column(db.Float(10,2),nullable=False)
-    password_hash=db.Column(db.String(128),nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.ForeignKey('user.id'))
+    nombre = db.Column(db.String(64), nullable=False)
+    tipo = db.Column(db.String(10), nullable=False)
+    pin = db.Column(db.String(10), nullable=False)
+    tActivo = db.Column(db.Float(10, 2), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
     def __repr__(self):
         return '<Recurso {}>'.format(self.nombre)
-    
+
     def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name!= 'password_hash'}
-       
-    def set_pass(self,p):
-        self.password_hash=generate_password_hash(p)
-    
-    def check_pass(self,p):
+        return {
+            c.name: getattr(self, c.name)
+            for c in self.__table__.columns if c.name != 'password_hash'
+        }
+
+    def set_pass(self, p):
+        self.password_hash = generate_password_hash(p)
+
+    def check_pass(self, p):
         return check_password_hash(self.password_hash, p)
--- app/routes.py
+++ app/routes.py
@@ -2,31 +2,36 @@ from flask import request
 from flask_restful import Resource, abort
 from functools import wraps
 from app import app, db, api
-from app.models import User,Recurso,Perms,Grupo
+from app.models import User, Recurso, Perms
 from app.token import Token
-localStorage={}
+localStorage = {}
+
 
 def isAuth(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if not "Authorization" in request.headers.keys():
             abort(401, message="Unauthorized")
-        t=Token()
+        t = Token()
         try:
-            localStorage['payload']=t.decode(request.headers['authorization'].split(' ')[1])
+            localStorage['payload'] = t.decode(
+                request.headers['authorization'].split(' ')[1])
         except Exception as e:
             abort(500, message=e)
         except TypeError as e:
             abort(403, message=e)
         return func(*args, **kwargs)
+
     return wrapper
+
 
 def isAdmin(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if not localStorage['payload']['type']=='admin':
-            abort(401,message="Unauthorized")
+        if not localStorage['payload']['type'] == 'admin':
+            abort(401, message="Unauthorized")
         return func(*args, **kwargs)
+
     return wrapper
 
 
@@ -46,7 +51,7 @@ class UserR(Resource):
         if not u:
             abort(404, message="El usuario no existe")
         return u.as_dict()
-    
+
     def post(self):
         username = request.json['username']
         email = request.json['email']
@@ -67,91 +72,86 @@ class LoginR(Resource):
     def post(self):
         email = request.json['email']
         passw = request.json['passw']
-        t=Token()
+        t = Token()
         try:
             u = User.query.filter_by(email=email).first()
         except Exception as e:
             abort(500, message="Error %s" % e)
         if not u:
-            abort(404,message="El usuario no existe")
+            abort(404, message="El usuario no existe")
         if not u.check_pass(passw):
             abort(401, message="La contraseña no es correcta")
-        return {'message':'Success!', 'token':t.encode(u.id, u.email, u.typ)}
-    
+        return {'message': 'Success!', 'token': t.encode(u.id, u.email, u.typ)}
+
     @isAuth
     def get(self):
-        t=Token()
-        userId=localStorage['payload']['sub']
+        t = Token()
+        userId = localStorage['payload']['sub']
         try:
             u = User.query.get(userId)
         except Exception as e:
             abort(500, message="Error %s" % e)
         if not u:
-            abort(404,message="El usuario no existe")
-        return {'message':'Success!', 'token':t.encode(u.id, u.email, u.typ)}
-
-
-class GrupoR(Resource):
-    def get(self, grupoId):
-        pass
-    def post(self):
-        pass
-    def put(self, grupoId):
-        pass
-    def delete(self, grupoId):
-        pass
+            abort(404, message="El usuario no existe")
+        return {'message': 'Success!', 'token': t.encode(u.id, u.email, u.typ)}
 
 
 class RecursoR(Resource):
     @isAuth
     def get(self, recursoId=False):
-        userId=localStorage['payload']['sub']
+        userId = localStorage['payload']['sub']
         try:
-            u=User.query.get(userId)
+            u = User.query.get(userId)
         except Exception as e:
             abort(500, message="Error %s" % e)
         if not u:
-            abort(404,message="El usuario no existe")
+            abort(404, message="El usuario no existe")
         try:
             if not recursoId:
-                p=Recurso.query.filter_by(userId=userId)
+                p = Recurso.query.filter_by(userId=userId)
             else:
-                p=Recurso.query.filter_by(userId=userId, id=recursoId).first()
+                p = Recurso.query.filter_by(userId=userId,
+                                            id=recursoId).first()
         except Exception as e:
             abort(500, message="Error %s" % e)
         if not p:
-            abort(404,message="El objeto no existe")
+            abort(404, message="El objeto no existe")
         if not recursoId:
-            return {'message':'Success', 'data':[i.as_dict() for i in p ]}
+            return {'message': 'Success', 'data': [i.as_dict() for i in p]}
         else:
-            return {'message':'Success', 'data':p.as_dict()}
-    
+            return {'message': 'Success', 'data': p.as_dict()}
+
     @isAuth
     def post(self):
-        userId=localStorage['payload']['sub']
+        userId = localStorage['payload']['sub']
         nombre = request.json['nombre']
-        permsId=request.json['permsId']
-        tipo=request.json['tipo']
-        tActivo=request.json['tActivo']
-        passw=request.json['passw']
-        pin=request.json['pin']
-        p=Recurso(userId=userId, nombre=nombre, tipo=tipo,pin=pin, tActivo=tActivo, permsId=permsId)
+        permsId = request.json['permsId']
+        tipo = request.json['tipo']
+        tActivo = request.json['tActivo']
+        passw = request.json['passw']
+        pin = request.json['pin']
+        p = Recurso(userId=userId,
+                    nombre=nombre,
+                    tipo=tipo,
+                    pin=pin,
+                    tActivo=tActivo,
+                    permsId=permsId)
         p.set_pass(passw)
         try:
             db.session.add(p)
             db.session.commit()
         except Exception as e:
             abort(500, message=f'Error: {e}')
-        return {'message':'Success!'}
-    
+        return {'message': 'Success!'}
+
     @isAuth
     def patch(self, recursoId):
-        passw=request.json['passw']
+        passw = request.json['passw']
         if not recursoId:
             abort(404, message="Debes ingresar una ID")
-        userId=localStorage['payload']['sub']
+        userId = localStorage['payload']['sub']
         try:
-            p=Recurso.query.filter_by(userId=userId, id=recursoId)
+            p = Recurso.query.filter_by(userId=userId, id=recursoId)
         except Exception as e:
             abort(500, message=f"Error {e}")
         if not p:
@@ -169,22 +169,23 @@ class RecursoR(Resource):
 
     @isAuth
     def delete(self, recursoId):
-        userId=localStorage['payload']['sub']
+        userId = localStorage['payload']['sub']
         try:
-            u=User.query.get(userId)
+            u = User.query.get(userId)
         except Exception as e:
             abort(500, message="Error %s" % e)
         if not u:
-            abort(404,message="El usuario no existe")
+            abort(404, message="El usuario no existe")
         try:
             if not recursoId:
-                p=Recurso.query.filter_by(userId=userId)
+                p = Recurso.query.filter_by(userId=userId)
             else:
-                p=Recurso.query.filter_by(userId=userId, id=recursoId).first()
+                p = Recurso.query.filter_by(userId=userId,
+                                            id=recursoId).first()
         except Exception as e:
             abort(500, message="Error %s" % e)
         if not p:
-            abort(404,message="El objeto no existe")
+            abort(404, message="El objeto no existe")
         try:
             if not recursoId:
                 for i in p:
@@ -194,42 +195,43 @@ class RecursoR(Resource):
             db.session.commit()
         except Exception as e:
             abort(500, message=f"Error: {e}")
-        return {'message':'Success!'}
+        return {'message': 'Success!'}
 
 
 class PermsR(Resource):
     @isAuth
     def get(self, recursoId):
-        userId=localStorage['payload']['sub']
+        userId = localStorage['payload']['sub']
         try:
             if not recursoId:
-                pm=Perms.query.filter_by(userId=userId)
+                pm = Perms.query.filter_by(userId=userId)
             else:
-                pm=Perms.query.filter_by(userId=userId,recursoId=recursoId)
+                pm = Perms.query.filter_by(userId=userId, recursoId=recursoId)
         except Exception as e:
             abort(500, message=f"Error {e}")
         if not pm:
             abort(404, message="No hay permisos")
-        return {'message':'Success!', 'data':[i.as_dict() for i in pm]}
+        return {'message': 'Success!', 'data': [i.as_dict() for i in pm]}
 
     @isAuth
     @isAdmin
     def post(self):
         pass
-    
+
     @isAuth
     @isAdmin
     def put(self, recursoId):
         pass
-    
+
     @isAuth
     @isAdmin
     def delete(self, recursoId):
-        userId=localStorage['payload']['sub']
+        userId = localStorage['payload']['sub']
         if not recursoId:
             abort(400, message='Bad Request')
         try:
-            pm=Perms.query.filter_by(userId=userId, recursoId=recursoId).first()
+            pm = Perms.query.filter_by(userId=userId,
+                                       recursoId=recursoId).first()
         except Exception as e:
             abort(500, message=f'Error {e}')
         if not pm:
@@ -238,12 +240,12 @@ class PermsR(Resource):
             db.session.delete(pm)
             db.session.commit()
         except Exception as e:
-            abort(500,message=f'Error {e}')
+            abort(500, message=f'Error {e}')
         return {'message': 'Success!'}
+
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(UserR, '/user/<string:userId>', '/user')
 api.add_resource(LoginR, '/login')
 api.add_resource(RecursoR, '/recurso/<string:recursoId>', '/recurso')
 api.add_resource(PermsR, '/perms/<string:recursoId>', '/perms')
-api.add_resource(GrupoR, '/grupo/<string:grupoId>', '/grupo')
